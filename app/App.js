fbdc23e743cca7c80f364c99ef0cb87f7c237b5f
Andrew Fitzpatrick <afitz@mindspring.com>
2015-12-14 20:57:31 -0800

    flickr api search

---
+++ app/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
+import Photos from './components/Photos';
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      photos: []
+    };
+    this.handleGetPhotos = this.handleGetPhotos.bind(this);
+  }
+
+  handleGetPhotos(photos){
+    this.setState({'photos': photos});
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <SearchBar _getPhotos={this.handleGetPhotos}/>
+        <Photos data={this.state.photos}/>
+      </div>
+    )
+  }
+};
+
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ app/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component{
+  render(){
+    return (
+      <header className="header">
+        <h3>React Flickr</h3>
+      </header>
+    )
+  }
+};
+
+export default Header;
